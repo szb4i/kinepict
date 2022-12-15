f9a47ad30e34b8db9590ac53484cd99e892da5f7
Szabolcs Nagy <szabolcs.nagy@pulispace.com>
2022-12-15 10:34:05 +0100

    moved unused convolution practice to convolution.py

--- convolution.py
+++ convolution.py
@@ -12,6 +12,21 @@ img = read_dva('./data/PATIENT_28_1.XA.0001.0001.2020.05.26.07.40.37.199459.1395
 
 kernel = get_gaussian_kernel()
 
+# a = np.arange(1,7)
+# b = np.array([
+#   np.arange(11,20),
+#   np.arange(21,30),
+#   np.arange(31,40),
+#   np.arange(41,50),
+#   np.arange(51,60),
+#   np.arange(61,70),
+#   np.arange(71,80),
+#   np.arange(81,90),
+#   np.arange(91,100),
+# ])
+# a_sliding_window = np.lib.stride_tricks.sliding_window_view(a, 3)
+# b_sliding_window = np.lib.stride_tricks.sliding_window_view(b, (3,3))
+
 ### method: numpy
 img_sliding_window = np.lib.stride_tricks.sliding_window_view(img, (3,3))
 # img_convolved = np.sum((img_sliding_window * kernel), axis=(2,3))
