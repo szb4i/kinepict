c633c355ccf8d0c20af0702b14a9b6b14137937f
Szabolcs Nagy <szabolcs.nagy@pulispace.com>
2022-12-15 08:54:49 +0100

    gaussain_kernel normalize boolean

--- utils/kernels/gaussian_kernel.py
+++ utils/kernels/gaussian_kernel.py
@@ -1,7 +1,10 @@
 import numpy as np
    
-def get_gaussian_kernel(l=3, sig=1.):
+def get_gaussian_kernel(l=3, sig=1., normalize=True):
     ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
     gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
     kernel = np.outer(gauss, gauss)
-    return kernel / np.sum(kernel)
+    if normalize:
+        return kernel / np.sum(kernel)
+    return kernel
+        
