f1034f2c9e777a04bfe2a78120892a950d3a8ab6
Szabolcs Nagy <szabolcs.nagy@pulispace.com>
2022-12-16 10:22:02 +0100

    method3: remove highest frequencies

--- method3.py
+++ method3.py
@@ -15,9 +15,9 @@ from utils.img.scaler import scale
 import cv2 as cv
 
 ### read dva
-img = read_dva('./data/PATIENT_28_1.XA.0001.0001.2020.05.26.07.40.37.199459.139512372.IMA')
+img = read_dva('./data/23_kep_test/Vena I/1_1_N')
 # np.savetxt('./outputs/img' + '.txt', img, delimiter='\t')
-plt.imshow(img, cmap='gray')
+# plt.imshow(img, cmap='gray')
 # plt.show()
 
 ### high frequency emphasis
@@ -26,49 +26,33 @@ u = np.fft.fftfreq(img.shape[1])
 vv, uu = np.meshgrid(v, u, indexing='ij')
 k1 = 0.5  
 k2 = 0.75
-### applying highfreq filter with different D0 values
-# D0 = 100
-# filter_f = k1 + k2*(1 - np.exp(-np.hypot(uu, vv)**2/(2*D0**2)))
-# img_f = np.fft.fft2(img)
-# img_filtered_s_0 = np.fft.ifft2(img_f*filter_f).real
-# D0 = 10
-# filter_f = k1 + k2*(1 - np.exp(-np.hypot(uu, vv)**2/(2*D0**2)))
-# img_f = np.fft.fft2(img)
-# img_filtered_s_1 = np.fft.ifft2(img_f*filter_f).real
-D0 = 1/10
-filter_f = k1 + k2*(1 - np.exp(-np.hypot(uu, vv)**2/(2*D0**2)))
-img_f = np.fft.fft2(img)
-img_filtered_s_2 = np.fft.ifft2(img_f*filter_f).real
-# D0 = 1/100
-# filter_f = k1 + k2*(1 - np.exp(-np.hypot(uu, vv)**2/(2*D0**2)))
-# img_f = np.fft.fft2(img)
-# img_filtered_s_3 = np.fft.ifft2(img_f*filter_f).real
+D0 = 0.05
+filter_f_1 = k1 + k2*(1 - np.exp(-np.hypot(uu, vv)**2/(2*D0**2)))
+img_f_1 = np.fft.fft2(img)
+img_filtered_s_1 = np.fft.ifft2(img_f_1*filter_f_1).real
+img_filtered_s_1 = scale(img_filtered_s_1)
+img_filtered_s_1_equalized = exposure.equalize_adapthist(img_filtered_s_1,clip_limit=0.00015,nbins=26200)
+img_filtered_s_1_equalized_gamma = img_filtered_s_1_equalized**1.2
+
+filter_f_2 = np.where(np.hypot(uu,vv) < 0.4, k1 + k2*(1 - np.exp(-np.hypot(uu, vv)**2/(2*D0**2))), 0)
+img_f_2 = np.fft.fft2(img)
+img_filtered_s_2 = np.fft.ifft2(img_f_2*filter_f_2).real
+img_filtered_s_2 = scale(img_filtered_s_2)
+img_filtered_s_2_equalized = exposure.equalize_adapthist(img_filtered_s_2,clip_limit=0.00015,nbins=26200)
+img_filtered_s_2_equalized_gamma = img_filtered_s_2_equalized**1.2
+
+# plt.imshow(np.fft.fftshift(filter_f_2), cmap='gray')
 
 # https://docs.opencv.org/4.x/d5/daf/tutorial_py_histogram_equalization.html
 # img_filtered_s_8bit = ((img_filtered_s/img_filtered_s.max())*255).astype(np.uint8)
 # clahe1 = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 # cl1 = clahe1.apply(img_filtered_s_8bit)
 
-# plt.figure(figsize=(12,7))
-# plt.subplot(1,4,1)
-# plt.imshow(img_filtered_s_0, cmap='gray')
-# plt.subplot(1,4,2)
-# plt.imshow(img_filtered_s_1, cmap='gray')
-# plt.subplot(1,4,3)
-# plt.imshow(img_filtered_s_2, cmap='gray')
-# plt.subplot(1,4,4)
-# plt.imshow(img_filtered_s_3, cmap='gray')
-# plt.show()
+plt.figure(figsize=(12,7))
+plt.subplot(1,2,1)
+plt.imshow(img_filtered_s_1_equalized, cmap='gray')
+plt.subplot(1,2,2)
+plt.imshow(img_filtered_s_2_equalized, cmap='gray')
+plt.show()
 
-img_filtered_s_2 = scale(img_filtered_s_2)
-img_filtered_s_equalized = exposure.equalize_adapthist(img_filtered_s_2,clip_limit=0.00015,nbins=26200)
-img_filtered_s_equalized_gamma = img_filtered_s_equalized**1.2
-
-# plt.figure(figsize=(12,7))
-# plt.subplot(1,2,1)
-# plt.imshow(img, cmap='gray')
-# plt.subplot(1,2,2)
-# plt.imshow(img_filtered_s_equalized, cmap='gray')
-# plt.show()
-
-np.savetxt('./outputs/method3' + '.txt', img_filtered_s_equalized_gamma, delimiter='\t')
+# np.savetxt('./outputs/method3' + '.txt', img_filtered_s_equalized_gamma, delimiter='\t')
