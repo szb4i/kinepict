810aee4870c381f65b0c87a9d38b78da8e006043
Szabolcs Nagy <szabolcs.nagy@pulispace.com>
2022-12-20 09:28:11 +0100

    method_3 with params

--- src/methods/method3.py
+++ src/methods/method3.py
@@ -3,14 +3,11 @@ from skimage import exposure
 
 from utils.img.scaler import scale
 
-def apply_method3(img):
+def apply_method3(img, k1=0.5, k2=0.75, D0=0.1):
   ### high frequency emphasis
   v = np.fft.fftfreq(img.shape[0])
   u = np.fft.fftfreq(img.shape[1])
   vv, uu = np.meshgrid(v, u, indexing='ij')
-  k1 = 0.5  
-  k2 = 0.75
-  D0 = 1/10
   filter_f = k1 + k2*(1 - np.exp(-np.hypot(uu, vv)**2/(2*D0**2)))
   img_f = np.fft.fft2(img)
   img_filtered_s_2 = np.fft.ifft2(img_f*filter_f).real
